0995d1e009d7567f1d7ac7ab38f83ce8eac4b808
Kush Daga <kushdaga@hotmail.com>
2020-09-29 17:29:52 +0530

    fixing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import React, { useEffect } from "react";
-
-import Faker from "faker";
+import React from "react";
 
 function App() {
-  const wordList = [];
-  useEffect(() => {
-    for (let i = 0; i < 500; i++) {
-      wordList.push(Faker.random.word());
-    }
-    console.log(wordList);
-  }, []);
   return <div>Word List</div>;
 }
 
